5166efdf17c433a4ffb6f6385211c7853504833c
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2018-06-11 21:50:08 +0800

    add counter rewrite to container pattern

--- 08-basic-counter/src/CounterContainer.js
+++ 08-basic-counter/src/CounterContainer.js
@@ -1,1 +1,48 @@
 // Intentionally left blank.
+import React, { Component } from 'react';
+import Counter from './Counter'
+export default class CounterContainer extends Component {
+  state={
+    counter:0
+  }
+  handleIncrement =()=>{
+    this.setState((state)=>{
+      return {
+        counter: state.counter+1
+      }
+    })
+    this.setState((state)=>{
+      return {
+        counter: state.counter+1
+      }
+    })
+    this.setState((state)=>{
+      return {
+        counter: state.counter+1
+      }
+    })
+
+    console.log(this.state.counter)
+  }
+  handleDecrement =()=>{
+    this.setState({
+      counter: this.state.counter-1
+    })
+  }
+  
+  handleReset =()=>{
+    this.setState({
+      counter: 0
+    })
+  }
+  render() {
+    return (
+      <Counter 
+        handleIncrement={this.handleIncrement}
+        handleDecrement={this.handleDecrement}
+        handleReset = {this.handleReset}
+        counter={this.state.counter}
+      />
+    );
+  }
+}
